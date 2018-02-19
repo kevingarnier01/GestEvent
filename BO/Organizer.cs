f2369da144ad70b70a2ad25d3cfdc9f155df78a1
barbara <barbara.krafft@gmail.com>
2018-02-19 15:31:51 +0100

    start Organizer  Entitie

--- BO/Organizer.cs
+++ BO/Organizer.cs
@@ -8,5 +8,10 @@ namespace BO
 {
     class Organizer
     {
+        public int Id { get; set; }
+
+        public String Login { get; set; }
+
+        public String Password { get; set; }
     }
 }
