6c3706d85896f89211e62cef097dc84de3914e94
Garnier Kévin <kevin.garnier2016@campus-eni.fr>
2018-02-20 13:52:42 +0100

    2eme commit Kévin
    Première page Convive

---
+++ GestEvent/Controllers/ConviveController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestEvent.Controllers
+{
+    public class ConviveController : Controller
+    {
+        // GET: Convive
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
