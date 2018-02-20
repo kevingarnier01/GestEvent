30ef1defff093c1b5352ac9eca45f4e108aa1174
barbara <barbara.krafft@gmail.com>
2018-02-20 09:34:51 +0100

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 89069344c3ec972cfab325a85b66ebb41e91e143.

---
+++ BO/Parking.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    class Parking
+    {
+        public int Id { get; set; }
+
+        public String Name { get; set; }
+
+        public List<Double> Coordinates { get; set; }
+
+        public int TotalPlaces { get; set; }
+
+        public int FreePlaces { get; set; }
+
+    }
+}
---
+++ BO/Theme.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    class Theme
+    {
+        public int Id { get; set; }
+
+        public String Name { get; set; }
+    }
+}
